3206b7a9388567ebdd9e694bd3fdd8216feda704
John Ivison <john@ivison.org>
2020-03-25 00:15:20 -0700

    Add CreateArtistWithImages query

--- api/resolvers/artist.js
+++ api/resolvers/artist.js
@@ -31,6 +31,26 @@ module.exports = {
   mutations: {
     createArtist: create(Artist, artistIncludes, artistTransformers),
     updateArtist: update(Artist, artistIncludes, artistTransformers),
-    deleteArtist: destroy(Artist)
+    deleteArtist: destroy(Artist),
+    createArtistWithImages: async (_, values) => {
+      let newValues = Object.assign({}, values);
+      delete newValues.icon;
+      delete newValues.banner;
+      delete newValues.cardTall;
+      delete newValues.cardFlat;
+      let artist = await create(
+        Artist,
+        artistIncludes,
+        artistTransformers
+      )(_, values);
+      await update(ArtistImages)(_, {
+        id: artist.images.id,
+        icon: values.icon,
+        banner: values.banner,
+        cardTall: values.cardTall,
+        cardFlat: values.cardFlat
+      });
+      return await one(Artist, artistIncludes)(_, { id: artist.id });
+    }
   }
 };
