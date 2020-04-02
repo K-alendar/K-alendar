5c221f1fbb8275e5b372f7a6345360c427eab441
John Ivison <john@ivison.org>
2020-04-01 22:27:49 -0700

    Add unique constraint to company.name

--- api/database/migrations/20200315224603-create-company.js
+++ api/database/migrations/20200315224603-create-company.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Companies', {
+    return queryInterface.createTable("Companies", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -9,7 +9,8 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        unique: true
       },
       createdAt: {
         allowNull: false,
@@ -22,6 +23,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Companies');
+    return queryInterface.dropTable("Companies");
   }
 };
