90fc0bdaa281edec8b8c6d011bb4260e434602ff
John Ivison <john@ivison.org>
2020-04-02 21:47:27 -0700

    Fix parsedate transformer bug

--- api/resolvers/utils/transformers.js
+++ api/resolvers/utils/transformers.js
@@ -1,17 +1,19 @@
+const moment = require('moment')
+
 module.exports = {
   parseDate: key => {
     return values => {
       let convertedDate = {};
       convertedDate[key] = values[key];
-      if (convertedDate[key] && convertedDate[key].class === String) {
+      if (convertedDate[key] && typeof convertedDate[key] === 'string') {
         convertedDate[key] = moment(
-          convertedDate[key],
-          "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+          convertedDate[key]
         ).unix();
       }
-      return { ...values, convertedDate };
+      return { ...values, ...convertedDate };
     };
   },
+  
   multi: (...transformers) => {
     return values => {
       let persistentValues = values;
